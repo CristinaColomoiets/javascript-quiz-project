6269595ce7ce567471d7ac288336d7b082900e70
CristinaColomoiets <colomoetscristina@gmail.com>
2024-04-15 17:17:07 +0200

    part 1 semi-done

--- src/question.js
+++ src/question.js
@@ -1,7 +1,21 @@
+// 
 class Question {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (text, choices, answer, difficulty)
+   constructor(textValue, choicesValue, answerValue, difficultyValue){
+    this.text = textValue;
+    this.choices = choicesValue;
+    this.answer = answerValue;
+    this.difficulty = difficultyValue;
+   }
 
-    // 2. shuffleChoices()
+   shuffleChoices(){
+    this.choices.forEach((choice, index) => {
+        const randomIndex = Math.floor(Math.random() * (index + 1));
+        [this.choices[index], this.choices[randomIndex]] = [this.choices[randomIndex], this.choices[index]];
+    });
+
+
+   }
 }
+
+const one_question = new Question("firstArgument", ['choice1', 'choice2', 'choice3', 'choice4'], true, '5');
+one_question.shuffleChoices();
--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,45 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
 
-    // 2. getQuestion()
+    constructor(questionsValue, timeLimitValue, timeRemainingValue){
+        this.questions = questionsValue;
+        this.timeLimit = timeLimitValue;
+        this.timeRemaining = timeRemainingValue;
+        this.correctAnswers = 0;
+        this.currentQuestionIndex = 0;
+    }
+
+
+    // should return the item from the 'questions' array at the position of 'currentQuestionIndex'
+    //  debe devolver el elemento del array "questions" en la misma posición de valor  "currentQuestionIndex".
+    getQuestion(){
+       return this.questions[this.currentQuestionIndex];  
+    }
+
+    moveToNextQuestion(){
+        return this.currentQuestionIndex++
+    }
+
+    shuffleQuestions(){
+        this.questions.forEach((question, index) => {
+            const randomIndex = Math.floor(Math.random() * (index + 1));
+            [this.questions[index], this.questions[randomIndex]] = [this.questions[randomIndex], this.questions[index]];
+        });
+    }
+
+
+    // debe incrementar 'correctAnswers' en 1 cuando se pasa una respuesta correcta como argumento
+    //    debe comprobar si la respuesta es correcta comparándola con la propiedad 'answer' de la pregunta actual
+    checkAnswer(answer){
+        if(this.questions[this.currentQuestionIndex] === answer){
+            this.correctAnswers++
+        }
+    }
+
+}
     
-    // 3. moveToNextQuestion()
-
-    // 4. shuffleQuestions()
+        
+   
 
     // 5. checkAnswer(answer)
 
     // 6. hasEnded()
-}
